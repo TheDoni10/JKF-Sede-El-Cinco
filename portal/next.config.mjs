a64e14b64c46574e4c92fe1df5601c60630a841a
Ministerio De Restauración <donibeltran46@gmail.com>
2026-06-23 00:15:58 -0500

    Delete portal directory

--- portal/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const portalBasePath = process.env.NEXT_PUBLIC_PORTAL_BASE_PATH ?? "/portal";
-
-const nextConfig = {
-  reactStrictMode: true,
-  ...(portalBasePath ? { basePath: portalBasePath } : {}),
-};
-
-export default nextConfig;
