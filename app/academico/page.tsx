ce86c5bfa39df9f365e8497a8b5253945b650acb
Ministerio De Restauración <donibeltran46@gmail.com>
2026-06-22 23:38:16 -0500

    Delete app directory

--- app/academico/page.tsx
+++
@@ -1,5 +0,0 @@
-import AcademicPortal from "./AcademicPortal";
-
-export default function AcademicPage() {
-  return <AcademicPortal />;
-}
--- app/api/academic/logout/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { clearAcademicCookie, STUDENT_COOKIE, TEACHER_COOKIE } from "@/lib/session";
-
-export async function POST() {
-  const response = NextResponse.json({ ok: true });
-  clearAcademicCookie(response, STUDENT_COOKIE);
-  clearAcademicCookie(response, TEACHER_COOKIE);
-  return response;
-}
